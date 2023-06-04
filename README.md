51b98984840adef918d7b97f30439da975dc8c49
Philipp Kapinos <blipkap@gmail.com>
2023-06-04 12:03:55 +0200

    added toggle function

--- README.md
+++ README.md
@@ -9,8 +9,12 @@ Checkout this repo.
 Add the following line to your init.el
 ```
 (load "/your/path/to/org-mermaid-render.el")
+(add-hook 'after-change-functions #'mermaid-org-babel-execute-on-change)
 ```
 
 /your/path/to must be replaced with your local pathing to this repo.
 
+~add hook~ activates the toggle to render your mermaid diagrams automatically. 
+
+to toggle use M-x mermaid-org-render-toggle
 
--- org-mermaid-render.el
+++ org-mermaid-render.el
@@ -1,22 +1,38 @@
 ;; Author: Philipp Kapinos
-;; Version: 0.0.1
+;; Version: 0.0.2
 
 (defvar m-org-babel-timer nil
 "Timer for delaying the execution")
 
 (defun mermaid-org-babel-execute-on-change (beg end _)
-	"Execute the current Org Babel code block and update the Mermaid diagram on buffer change."
+  "Execute the current Org Babel code block and update the Mermaid diagram on buffer change."
     (when (and (eq major-mode 'org-mode)
 	       (org-in-src-block-p)
 	       (string= (org-element-property :language (org-element-context)) "mermaid"))
-        (when m-org-babel-timer
-			(cancel-timer m-org-babel-timer))
-        (setq m-org-babel-timer (run-with-idle-timer 1 nil
-			#'mermaid-org-babel-execute-and-display beg end))))
+      (mermaid-org-timer)))
+       
+(defun mermaid-org-timer ()
+  (when m-org-babel-timer
+    (cancel-timer m-org-babel-timer))
+  (setq m-org-babel-timer (run-with-idle-timer 1 nil #'mermaid-org-babel-execute-and-display)))
 
-(defun mermaid-org-babel-execute-and-display (beg end)
+(defun mermaid-org-babel-execute-and-display ()
 	"Execute the Org Babel code block and display the Mermaid diagram."
     (org-babel-execute-src-block)
     (org-redisplay-inline-images))
 
-(add-hook 'after-change-functions #'mermaid-org-babel-execute-on-change)
+(defvar mermaid-org-babel-execute-enabled t
+  "Variable for activation/deactivation of `mermaid-org-babel-execute-on-change'.")
+
+(defun mermaid-org-render-toggle ()
+  "Function to switch hook `mermaid-org-babel-execute-on-change'."
+  (interactive)
+  (setq mermaid-org-babel-execute-enabled (not mermaid-org-babel-execute-enabled))
+  (if mermaid-org-babel-execute-enabled
+      (progn
+	(add-hook 'after-change-functions #'mermaid-org-babel-execute-on-change)
+	(message "Hook added for auto rendering"))
+    (progn
+      (remove-hook 'after-change-functions #'mermaid-org-babel-execute-on-change)
+      (message "Hook removed for auto rendering"))))
+
